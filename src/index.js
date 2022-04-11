49f6fd3901614768c882504ab4d0752a1ccdc745
Niladri Sekhar Mukherjee <niladrisekharmukherjee3199@gmail.com>
2022-04-11 21:15:05 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -2,14 +2,22 @@ const express = require('express');
 var bodyParser = require('body-parser');
 
 const route = require('./routes/route.js');
+const { default: mongoose } = require('mongoose');
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+mongoose.connect("mongodb+srv://REYNIL310609:OnIYmcfVuOkV0Dkr@cluster0.csvzw.mongodb.net/REYNIL310609?retryWrites=true&w=majority",{
+    useNewurlParser:true
+})
+.then( () => console.log("MangoDb is connected"))
+.catch ( err => console.log(err) )
+
 
 app.use('/', route);
 
 app.listen(process.env.PORT || 3000, function() {
     console.log('Express app running on port ' + (process.env.PORT || 3000))
 });
+
